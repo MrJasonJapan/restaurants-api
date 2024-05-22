b1e46a3214ac83b96c9c2dca2d1b9cbf621f3364
Jason Gettys <jason@digital-knowledge.co.jp>
2024-05-22 20:22:50 +0900

    Refactor: Make DbContext Accessible from Presentation Layer
    - Made the `DbContext` visible from the Presentation Layer (Restaurants.API).
    - Managed the database connection string inside the app settings file.
    
    Changes:
    - Updated `appsettings.Development.json` to include connection strings.
    - Modified `Program.cs` in Restaurants.API to configure the DbContext.
    - Updated `Restaurants.API.csproj` to include necessary project references and packages.
    - Refactored `RestaurantsDbContext` to use dependency injection.
    - Added `ServiceCollectionExtensions` to the infrastructure layer to manage service registrations.

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,8 +1,12 @@
+using Restaurants.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
--- Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -3,18 +3,12 @@ using Restaurants.Domain.Entities;
 
 namespace Restaurants.Infrastructure.Persistence;
 
-internal class RestaurantsDbContext : DbContext
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
 {
     internal DbSet<Restaurant> Restaurants { get; set; }
 
     internal DbSet<Dish> Dishes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RestaurantsDb;Trusted_Connection=True;");
-    }
-
-    // onmodelcreating
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
