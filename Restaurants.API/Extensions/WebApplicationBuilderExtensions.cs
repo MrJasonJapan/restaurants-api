5d86c66e88c9fcff8cdd3aaabd24a3e95d0225cb
Jason Gettys <jason@digital-knowledge.co.jp>
2024-06-14 19:28:57 +0900

    Refactor Program.cs to Improve Presentation Layer Setup (added file I forgot to commit)
    - Replaced all presentation layer setups with the `AddPresentation` method (including Swagger and Serilog) for enhanced modularity and maintainability.
    
    Other notes / Take-aways
    
    - The `AddPresentation` method encapsulates all presentation setups, promoting cleaner and more maintainable code.
    - Streamlining configurations out of `Program.cs` makes the startup process simpler and less error-prone.

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,41 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+namespace restaurants_api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            // Allow swagger to show a UI element to save a token as specified by the user.
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme()
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            // Add setting that will map the "bearerAuth" security definition to all endpoints, 
+            // so that swagger will know to include the UI-saved token in each request.
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            },
+            []
+        }
+            });
+        });
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
+        );
+
+    }
+}
